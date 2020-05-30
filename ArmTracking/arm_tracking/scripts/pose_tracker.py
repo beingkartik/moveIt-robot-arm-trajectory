02ede8664926dc76829c5e82f84592ed50b2a9e4
Kartik Gupta <kartikg94@gmail.com>
2020-05-30 03:36:31 -0700

    Add files via upload

--- ArmTracking/arm_tracking/scripts/pose_tracker.py
+++ ArmTracking/arm_tracking/scripts/pose_tracker.py
@@ -4,14 +4,18 @@ Created on Sun May 17 14:03:33 2020
 
 @author: kartik
 """
+from __future__ import print_function
 import numpy as np
 #import pyquaternion as pyq
-import cv2
+import sys
 from cv_bridge import CvBridge,CvBridgeError
 import rospy
 from sensor_msgs.msg import Image
 from arm_tracking_planner_executer import Robot
 from environment import Environment
+from calibration_transforms import Transforms
+if  '/opt/ros/kinetic/lib/python2.7/dist-packages' in sys.path : sys.path.remove('/opt/ros/kinetic/lib/python2.7/dist-packages') 
+import cv2
 from cv2 import aruco
 
 class PoseTracker():
@@ -19,20 +23,26 @@ class PoseTracker():
         self.robot = robot
         self.env = env
         self.bridge = CvBridge()
-        self.image_sub = rospy.Subscriber('/camera/color/image_raw', Image, self.get_image)
+        self.image_sub = rospy.Subscriber('/usb_cam/image_raw', Image, self.get_image)
         self.image = None
+        
+        #put camera matrices here
         self.camera_instrinsic = np.array(((617.0026849655,-0.153855356,315.5900337131),#fx, s,cx
                    (0,614.4461785395,243.0005874753), ##0,fy,cy
                    (0,0,1) ))
         self.dist = np.array((0.1611730644,-0.3392379107,0.0010744837,	0.000905697)) #k1,k2,p1,p2 ie radial dist and tangential dist
+        
         self.marker_side = 0.039
-
+        
+        #aruco marker tracks all the markers in the frame and returns poses as list, specify which pose belongs to what 
+        self.robot_base_id,self.robot_ef_id, self.workpiece_id = 0,1,2
+        
+        
     def get_image(self,Image):
         try:
             self.image = self.bridge.imgmsg_to_cv2(Image,desired_encoding = 'bgr8')
-            
         except CvBridgeError as e:
-            print e
+            print (e)
 
     def estimate_pose(self,frame, marker_side, camera_instrinsic,dist):
 
@@ -48,28 +58,53 @@ class PoseTracker():
             if id_ is not None:
             # estimate pose of each marker and return the values
             # rvet and tvec-different from camera coefficients
-                rvec, tvec = aruco.estimatePoseSingleMarkers(corners[id_[0]], marker_side, camera_instrinsic, dist)
-                output.append([rvec,tvec,corners])
+                rvec, tvec, _ = aruco.estimatePoseSingleMarkers(corners[id_[0]], marker_side, camera_instrinsic, dist)
                 
+                output.append([rvec,tvec,corners])
+
         return output
     
     def get_robot_and_workpiece_pose(self):
         try:
             poses = self.estimate_pose(self.image,self.marker_side,self.camera_instrinsic,self.dist)
-            if len(poses) != 2:
+            if len(poses) != 3:
                 rospy.logerr('Only '+ str(len(poses))+' markers visible in the image')
+            else:
+                rospy.loginfo("SUCCEESSSS")
         except:
             poses = []
             rospy.logerr('Not able to track')
+    
+    
+    def calibrate_transforms(self):
+        try:
+            poses = self.estimate_pose(self.image,self.marker_side,self.camera_instrinsic,self.dist)
+            rvec_rb = poses[self.robot_base_id][0]
+            R_rb = cv2.Rodrigues(rvec_rb)
+            self.transform = Transforms(R_rb)
+            rospy.loginfo(self.transform.R_cb)
+        except:
+            rospy.logerr('Not able to track')
             
     
-    
+    #get robot ef position (x,y,z) in robot frame using camera tracking
+    def get_robot_ef_position(self):
+        poses = self.estimate_pose(self.image,self.marker_side,self.camera_instrinsic,self.dist)
+        p_cr = poses[self.robot_ef_id][1] #get tvec from aruco marker
+        return self.transform.get_pos_rframe(p_cr)
+        
+    #gets robot ef pose(6D) in robot frame using moveit
     def get_robot_pose(self):
+
         robot_pose = self.robot.group.get_current_pose().pose.position
         robot_pose = np.array((robot_pose.x,robot_pose.y,robot_pose.z))
         return robot_pose
         
-    
+    def get_workpiece_marker_position(self):
+        poses = self.estimate_pose(self.image,self.marker_side,self.camera_instrinsic,self.dist)
+        p_cr = poses[self.workpiece_id][1] #get tvec from aruco marker
+        return self.transform.get_pos_rframe(p_cr)
+        
     def get_workpiece_edge(self):
         shape = self.env.workpiece_size
         target_end_points = np.linspace((0,shape[1],shape[2],1),(shape[0],shape[1],shape[2],1),10)
@@ -80,13 +115,16 @@ class PoseTracker():
         workpiece_edge_points = (np.dot(transformation_matrix,target_end_points.T)).T[:,:3]
         return workpiece_edge_points
     
+
 if __name__ == '__main__':
     robot = Robot('kinova')
     env = Environment()    
     posetracker = PoseTracker(robot,env)
     
     while(1):
-        posetracker.get_robot_and_workpiece_pose()
+        if posetracker.image is not None: 
+            posetracker.calibrate_transforms            
+            posetracker.get_robot_and_workpiece_pose()
 #    while(1):
 #        if posetracker.image is not None:
 ##            image = posetracker.image.copy()
--- ArmTracking/arm_tracking/scripts/trajectory_follower.py
+++ ArmTracking/arm_tracking/scripts/trajectory_follower.py
@@ -4,6 +4,7 @@ Created on Sun May 17 14:46:41 2020
 
 @author: kartik
 """
+from __future__ import print_function
 
 from arm_tracking_planner_executer import Robot
 from environment import Environment
@@ -11,24 +12,42 @@ from pose_tracker import PoseTracker
 from arm_pid import ArmPID
 import numpy as np
 import rospy
+import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
     robot = Robot('kinova')
     robot.set_planner_type('RRT')
     env = Environment()    
     pose_track = PoseTracker(robot,env)
+
     robot.get_and_add_box(env.workpiece_size,env.workpiece_pose,env.workpiece_shape)
-    pid_controller = ArmPID(robot,0,0,0,[1,0,0],0,pose_track)
+    
+    pid_controller = ArmPID(robot,0.1,0,0,[1,0,0],0.01,pose_track)
     target_trajectory = pose_track.get_workpiece_edge()
-    print "stat"
     for target in target_trajectory:
+        
+        print(target)
         waypoints = []        
         waypoints.append((robot.group.get_current_pose().pose))
         target_pose = [0,0,0,0,180,0]
         target_pose[:3] = target
         waypoints.append(target_pose)
             
-        pid_controller.point_pid_jacobian(target)
+        pid_controller.point_pid_cartesian(target)
+#        plan = robot.get_plan_move_along_line(waypoints)
+#        robot.execute_trajectory(plan)
+        rospy.loginfo(pid_controller.get_error(target))
+    
+    while(1):
+        target_pose = robot.group.get_random_pose()
+        print(target_pose)
+        waypoints = []        
+        waypoints.append((robot.group.get_current_pose().pose))
+#        target_pose = [0,0,0,0,180,0]
+#        target_pose[:3] = target
+        waypoints.append(target_pose)
+            
+        pid_controller.point_pid_cartesian(target_pose)
 #        plan = robot.get_plan_move_along_line(waypoints)
 #        robot.execute_trajectory(plan)
         rospy.loginfo(pid_controller.get_error(target))
